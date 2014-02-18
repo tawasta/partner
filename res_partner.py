461158c1cf4263954dcaaa12f28a75f59fd2a2e4
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-02-18 13:42:33 +0200

    Add a module

---
+++ res_partner.py
@@ -0,0 +1,25 @@
+from openerp.osv import osv, fields
+from tools.translate import _
+
+class ResPartner(osv.Model):
+    _name = "res.partner"
+    _inherit = "res.partner"
+
+    _columns = {
+        'customer_of': fields.many2many(
+            'res.company',
+            'res_company_customer_rel'
+            '',
+            'id',
+            string=_('Customer of')
+        ),
+        'supplier_of': fields.many2many(
+            'res.company',
+            'res_company_supplier_rel'
+            '',
+            'id',
+            string=_('Supplier of')
+        ),
+    }
+    
+ResPartner()
