3a4ecf5df30f1aab199b24149328800ed14010b0
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-03-06 13:11:34 +0200

    Add a model res_partner

---
+++ res_partner.py
@@ -0,0 +1,23 @@
+from openerp.osv import osv, fields
+from tools.translate import _
+
+class ResPartner(osv.Model):
+    _name = "res.partner"
+    _inherit = "res.partner"
+
+    def _get_selection(self, cursor, user_id, context=None):
+        return (
+            ('allowed', _('Allowed')),
+            ('ask', _('Confirm by case')),
+            ('no', _('Not allowed'))
+        )
+
+    _columns = {
+        'referenceright': fields.selection( _get_selection, 'Reference right' )
+    }
+    
+    _defaults = {
+        'referenceright': False,             
+    }
+    
+ResPartner()
