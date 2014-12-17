cb6ac7d739c504cd9c41e98838d9b27c31c2bd1b
Jarmo Kortetjärvi <jarmo@futurable.fi>
2014-12-17 09:51:02 +0200

    Override search method to only allow one refright per company

--- reference_right.py
+++ reference_right.py
@@ -1,5 +1,6 @@
 from openerp.osv import osv, fields
 from openerp import SUPERUSER_ID
+from openerp.tools.translate import _
 import logging
 _logger = logging.getLogger(__name__)
 
@@ -10,9 +11,9 @@ class Reference_right(osv.Model):
     ''' When module is installed, fetch all companies and create reference rights '''
     def _init_reference_rights(self, cr, uid, ids=None, context=None):
         ref_rights = {
-            'reference_right_allowed': 'Allowed',
-            'reference_right_ask': 'Ask',
-            'reference_right_no': 'No',
+            'reference_right_allowed': _('Allowed'),
+            'reference_right_ask': _('Ask'),
+            'reference_right_no': _('No Right'),
         }
 
         company_obj = self.pool.get('res.company')
@@ -52,11 +53,28 @@ class Reference_right(osv.Model):
             ids = self.search(cr, uid, args, limit=limit, context=context)
         return self.name_get(cr, uid, ids, context)
     
+    def search(self, cr, uid, args, offset=0, limit=None, order=None, context=None, count=False):
+        _logger.warn(args)
+        refright_ids = args[0][2]
+        refrights = self.browse(cr, uid, refright_ids, context)
+        
+        company_ids = []
+        for refright in refrights:
+            company_ids.append(refright.company_id.id)
+        
+        args = [['company_id', 'not in', company_ids]]
+
+        return super(Reference_right, self).search(cr, uid, args, offset, limit, order, context=context, count=count)
+    
     _columns = {
         'name': fields.char(string='Reference_right', size=128),          
         'code': fields.char(string='Code', size=128),          
         'company_id': fields.many2one('res.company', 'Company'),
         'partner_ids': fields.many2many('res.partner', id1='referenceright', id2='partner_id', string='Partners'),
     }
+    
+    _sql_constraints = [
+        ('company_code_unique', 'unique(company_id, code)', 'This company already has a reference right with this code.')
+    ]
     
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
