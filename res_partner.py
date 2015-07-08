b1dfc493d3d634cb9a4e3ebd73eb729af8b5b8db
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-07-08 10:17:35 +0300

    Refactor to follow PEP8

--- res_partner.py
+++ res_partner.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-from openerp import models, api, fields
+from openerp import models, fields
+
 
 class ResPartner(models.Model):
     _inherit = "res.partner"
-    
-    note_ids = fields.One2many("res.partner.note", 'partner_id', 'Internal notes')
+
+    note_ids = fields.One2many("res.partner.note", 'partner_id',
+                               'Internal notes')
--- res_partner_note.py
+++ res_partner_note.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from openerp import models, api, fields
 
+
 class InternalNote(models.Model):
     _name = "res.partner.note"
     _order = "create_date desc"
-    
+
     active = fields.Boolean("Active", default=True)
     name = fields.Char("Title", size=256)
     note = fields.Text("Note")
@@ -13,7 +14,7 @@ class InternalNote(models.Model):
     @api.multi
     def unlink(self):
         ''' Deactivates the note instead of deleting '''
-        
+
         self.active = False
-        
+
         return True
