4d7f56329221867c07b492e69c1fd8bd78b88d49
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-08-14 16:28:33 +0300

    Fix a inheritance issue

--- sale_order.py
+++ sale_order.py
@@ -5,15 +5,14 @@ class SaleOrder(osv.Model):
     _inherit = "sale.order"
     
     # When quotation is about to be set to "In finalization"
+
     def action_in_finalization(self, cr, uid, ids, context=None):
         # Update the "customer of" field
         sale_order = self.browse(cr, uid, ids, context)[0]
         res_partner = self.pool.get('res.partner')
         
         res_partner.write(cr, uid, sale_order.partner_id.id, {'customer_of': [(4, sale_order.company_id.id)]}, context=context)
-    
-        # Update the state
-        res = self.write(cr, uid, ids, {'state': 'in_finalization'}, context=context)
-        return None
-    
-SaleOrder()
+        
+        res = super(SaleOrder, self).action_in_finalization(cr, uid, ids, context)
+
+        return res
