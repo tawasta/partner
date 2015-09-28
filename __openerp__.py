e6fcfafb0bd26b8bbf59d1e12c107941151c4e3b
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-09-28 16:14:58 +0300

    Remove an incompatible model

--- __openerp__.py
+++ __openerp__.py
@@ -21,7 +21,7 @@
 {
     'name': 'Partner Multicompany Extension',
     'category': 'CRM',
-    'version': '8.0.0.5.3',
+    'version': '8.0.0.6.0',
     'author': 'Oy Tawasta Technologies Ltd.',
     'website': 'http://www.tawasta.fi',
     'depends': ['crm'],
