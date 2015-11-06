125b411c01ea80a20a51771e9129c49f5b04148e
Jarmo Kortetjarvi <jarmo@futurable.fi>
2015-11-06 20:07:46 +0200

    Improve documentation

--- __openerp__.py
+++ __openerp__.py
@@ -1,25 +1,33 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#   Copyright (c) 2014 - Vizucom Oy (http://www.vizucom.com)
+#    Author: Jarmo Kortetjärvi
+#    Copyright 2015 Oy Tawasta OS Technologies Ltd. (http://www.tawasta.fi)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
 #
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
 #
 ##############################################################################
 {
-    'name': 'Partner reference right',
+    'name': 'Partner Reference Right',
     'category': 'Sales',
-    'version': '0.2',
-    'author': 'Vizucom Oy',
-    'website': 'http://www.vizucom.com',
+    'version': '8.0.0.2.1',
+    'author': '''
+Vizucom Oy,
+Oy Tawasta OS Technologies Ltd.
+''',
+    'license': 'AGPL-3',
     'depends': [],
-    'description': "Adds a reference right field for partners (customers)",
     'installable': True,
     'data': [
          'data/reference_rights.xml',
