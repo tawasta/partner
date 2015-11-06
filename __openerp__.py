b9d048ffb965e141ac12d84bea2e5fb9204526b7
Jarmo Kortetjarvi <jarmo@futurable.fi>
2015-11-06 20:26:58 +0200

    Add the icon

--- __openerp__.py
+++ __openerp__.py
@@ -21,7 +21,7 @@
 {
     'name': 'Partner Reference Right',
     'category': 'Sales',
-    'version': '8.0.0.2.5',
+    'version': '8.0.0.2.6',
     'author': '''
 Vizucom Oy,
 Oy Tawasta OS Technologies Ltd.
