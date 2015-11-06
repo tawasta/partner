970056dba1f52dec5293faf1576f16a38db5261a
Jarmo Kortetjarvi <jarmo@futurable.fi>
2015-11-06 20:19:56 +0200

    Remove company name from tags when there is only one company

--- __openerp__.py
+++ __openerp__.py
@@ -21,7 +21,7 @@
 {
     'name': 'Partner Reference Right',
     'category': 'Sales',
-    'version': '8.0.0.2.3',
+    'version': '8.0.0.2.4',
     'author': '''
 Vizucom Oy,
 Oy Tawasta OS Technologies Ltd.
