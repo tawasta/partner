f74c8dd6f822bc7259287aae6b3d8fae25f50d73
Jarmo Kortetjarvi <jarmo@futurable.fi>
2015-11-07 11:03:36 +0200

    Improve fields usability

--- __openerp__.py
+++ __openerp__.py
@@ -21,7 +21,7 @@
 {
     'name': 'Partner Reference Right',
     'category': 'Sales',
-    'version': '8.0.0.2.8',
+    'version': '8.0.0.2.9',
     'author': '''
 Vizucom Oy,
 Oy Tawasta OS Technologies Ltd.
