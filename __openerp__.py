6fb71a45a481aad940c7db35ae5dd9427d083e7f
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-11-17 11:00:43 +0200

    Improve documentation

--- __openerp__.py
+++ __openerp__.py
@@ -1,27 +1,35 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#   Copyright (c) 2015 - Oy Tawasta Technologies Ltd. (http://www.tawasta.fi)
+#    Author: Jarmo Kortetjärvi
+#    Copyright 2015 Oy Tawasta OS Technologies Ltd. (http://www.tawasta.fi)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
 #
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
 #
 ##############################################################################
 {
     'name': 'Partner Internal Note Extension',
     'category': 'CRM',
     'version': '8.0.0.3.0',
-    'author': 'Oy Tawasta Technologies Ltd.',
     'website': 'http://www.tawasta.fi',
-    'depends': ['crm'],
-    'description': '''
-* Allows multiple internal notes with note titles and creator and editor information.
-''',
+    'author': 'Oy Tawasta Technologies Ltd.',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'crm'
+    ],
     'data': [
          'view/res_partner.xml',
          'view/partner_note.xml',
