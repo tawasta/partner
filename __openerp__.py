977d300b76bb02cc93eb0c722eea513c17a706d5
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-11-17 11:18:19 +0200

    Improve structure, add translations

--- __openerp__.py
+++ __openerp__.py
@@ -21,7 +21,7 @@
 {
     'name': 'Partner Internal Note Extension',
     'category': 'CRM',
-    'version': '8.0.0.3.2',
+    'version': '8.0.0.3.3',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
     'license': 'AGPL-3',
