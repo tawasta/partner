8336051e415331cba8cf10db201074c7ac65cf76
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-12-02 13:39:50 +0200

    Add translations

--- __openerp__.py
+++ __openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Partner Invoice Transmit Types',
-    'version': '8.0.0.4.1',
+    'version': '8.0.0.4.2',
     'category': 'Accounting & Finance',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
