6ab5747611ba426456feb3114ea4fdb9cf24317b
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2015-12-21 08:36:08 +0200

    Added classes segment and category

---
+++ models/__init__.py
@@ -0,0 +1,1 @@
+import business_segment, res_partner
---
+++ models/res_partner.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# 1. Standard library imports:
+
+# 2. Known third party imports:
+
+# 3. Odoo imports (openerp):
+from openerp import api, fields, models
+
+# 4. Imports from Odoo modules:
+
+# 5. Local imports in the relative form:
+
+# 6. Unknown third party imports:
+
+class Partner(models.Model):
+    
+    # 1. Private attributes
+    _inherit = 'res.partner'
+
+    # 2. Fields declaration
+    Business_segment = fields.Many2one('business_segment.segment', string='Business Segment')
+
+
+    # 3. Default methods
+
+    # 4. Compute and search fields, in the same order that fields declaration
+
+    # 5. Constraints and onchanges
+
+    # 6. CRUD methods
+
+    # 7. Action methods
+
+    # 8. Business methods
+    
