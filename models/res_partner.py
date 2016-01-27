d3d0079bf8d77d92002b5a839483700d9ad2becf
Jarmo Kortetjärvi <jarmo@futurable.fi>
2016-01-27 13:17:53 +0200

    Improve formatting

--- models/res_partner.py
+++ models/res_partner.py
@@ -5,5 +5,14 @@ from openerp import models, api, fields
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    customer_of = fields.Many2many('res.company', 'res_company_customer_rel', string='Customer of')
-    supplier_of = fields.Many2many('res.company', 'res_companyp_supplier_rel', string='Supplier of')
+    customer_of = fields.Many2many(
+        'res.company',
+        'res_company_customer_rel',
+        string='Customer of',
+    )
+
+    supplier_of = fields.Many2many(
+        'res.company',
+        'res_company_supplier_rel',
+        string='Supplier of'
+    )
