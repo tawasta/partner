42621c4edee4d75107681e63b9461f938949300b
Jarmo Kortetjarvi <jarmo@futurable.fi>
2016-01-28 20:30:23 +0200

    Add context to guardian/ward creation

--- __openerp__.py
+++ __openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Partner Guardians',
     'summary': 'Allows partner to have guardians',
-    'version': '8.0.0.2.1',
+    'version': '8.0.0.2.2',
     'category': 'CRM',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
