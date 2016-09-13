08eae0b4cd155fb5bdcf1ed87a6d552a3383e1e4
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2016-09-13 11:03:06 +0300

    Add new module for showing invoice transmit types on stock views

---
+++ partner_invoice_transmit_types_stock/__openerp__.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2016 Oy Tawasta OS Technologies Ltd. (http://www.tawasta.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    'name': 'Partner invoice transmit types to stock',
+    'summary': 'Show invoice transmit type from res_partner_invoice_transmit_types in stock',
+    'version': '8.0.0.1.0',
+    'category': 'Manufacturing',
+    'website': 'http://www.tawasta.fi',
+    'author': 'Oy Tawasta Technologies Ltd.',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'external_dependencies': {
+        'python': [],
+        'bin': [],
+    },
+    'depends': [
+        'res_partner_invoice_transmit_types',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
+
+##############################################################################
+#
+#    AVAILABLE CATEGORIES
+#
+#    Account Charts
+#    Accounting & Finance
+#    Association
+#    Administration
+#    CRM
+#    Events
+#    Extra Rights
+#    Human Resources
+#    Inventory
+#    Invoicing & Payments
+#    Lead Automation
+#    Localization
+#    Manufacturing
+#    Mass Mailing
+#    Other Extra Rights
+#    Point of Sale
+#    Project
+#    Purchases
+#    Sales
+#    Specific Industry Applications
+#    Survey
+#    Technical Settings
+#    Theme
+#    Website
+#
+##############################################################################
