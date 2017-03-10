6bd5350c80aa0e2adb42409037251d7eafc245fd
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-03-10 12:43:55 +0200

    Add better views, with address information

--- partner_newsletter_prints/models/newsletter_print.py
+++ partner_newsletter_prints/models/newsletter_print.py
@@ -24,11 +24,44 @@ class NewsletterPrint(models.Model):
     contact = fields.Many2one('res.partner', 'Contact', domain=[('is_company', '=', False)])
     amount = fields.Integer("Amount")
 
-    contact_name = fields.Char("Contact name")
+    contact_name = fields.Char("Contact name", compute='compute_contact_name')
+    contact_street = fields.Char("Contact address", compute='compute_contact_street')
+    contact_zip = fields.Char("Contact zip", compute='compute_contact_zip')
+    contact_city = fields.Char("Contact city", compute='compute_contact_city')
+    contact_country = fields.Char("Contact country", compute='compute_contact_country')
 
     # 3. Default methods
 
     # 4. Compute and search fields, in the same order that fields declaration
+    @api.multi
+    def compute_contact_name(self):
+        for record in self:
+            record.contact_name = record.contact.name
+
+    @api.multi
+    def compute_contact_street(self):
+        for record in self:
+            contact_street = record.contact.street
+
+            if record.contact.street2:
+             contact_street + " " + record.contact.street2
+
+            record.contact_street = contact_street
+
+    @api.multi
+    def compute_contact_zip(self):
+        for record in self:
+            record.contact_zip = record.contact.zip
+
+    @api.multi
+    def compute_contact_city(self):
+        for record in self:
+            record.contact_city = record.contact.city
+
+    @api.multi
+    def compute_contact_country(self):
+        for record in self:
+            record.contact_country = record.contact.country_id
 
     # 5. Constraints and onchanges
 
