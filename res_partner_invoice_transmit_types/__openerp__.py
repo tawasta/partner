5fbef2ed641148286ec5f1a17481cc3fd7e7f324
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-04-25 08:06:53 +0300

    Impove referenced field

--- res_partner_invoice_transmit_types/__openerp__.py
+++ res_partner_invoice_transmit_types/__openerp__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Partner Invoice Transmit Types',
-    'version': '8.0.0.4.6',
+    'version': '8.0.0.4.7',
     'category': 'Accounting & Finance',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
