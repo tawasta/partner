32478a1ffd54d61c787ca72f13c1030cd7d5c49b
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-05-15 15:58:21 +0300

    Improve readme

---
+++ res_partner_roles/README.rst
@@ -0,0 +1,47 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+=========================================
+Partner company, contact and server roles
+=========================================
+
+Adds improved tag-like roles for partners depending on if they are companies or contacts.
+Default tags are splitted in three:
+- company roles
+- contact roles
+- server roles
+
+Installation
+============
+
+Install the module form Settings->Local Modules
+
+Configuration
+=============
+\-
+
+Usage
+=====
+\-
+
+Known issues / Roadmap
+======================
+\-
+
+Credits
+=======
+
+Contributors
+------------
+
+* Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
+
+Maintainer
+----------
+
+.. image:: http://tawasta.fi/templates/tawastrap/images/logo.png
+   :alt: Oy Tawasta OS Technologies Ltd.
+   :target: http://tawasta.fi/
+
+This module is maintained by Oy Tawasta OS Technologies Ltd.
--- res_partner_roles/__openerp__.py
+++ res_partner_roles/__openerp__.py
@@ -1,28 +1,45 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#   Copyright (c) 2014 - Vizucom Oy (http://www.vizucom.com)
+#    Author: Oy Tawasta OS Technologies Ltd.
+#    Copyright 2016 Oy Tawasta OS Technologies Ltd. (http://www.tawasta.fi)
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
 #    License, or (at your option) any later version.
 #
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
 #
 ##############################################################################
+
 {
     'name': 'Partner Roles',
-    'category': 'Sales',
-    'version': '8.0.0.5.1',
-    'author': 'Vizucom Oy',
-    'website': 'http://www.vizucom.com',
-    'depends': ['crm'],
-    'description': "Adds tag-like roles for partners depending on if they are companies or contacts",
+    'summary': 'Adds tag-like roles for partners depending on if they are companies or contacts',
+    'version': '8.0.1.0.0',
+    'category': 'CRM',
+    'website': 'https://github.com/Tawasta/partner',
+    'author': 'Oy Tawasta Technologies Ltd.',
+    'license': 'AGPL-3',
+    'application': False,
     'installable': True,
+    'external_dependencies': {
+        'python': [],
+        'bin': [],
+    },
+    'depends': [
+        'crm',
+    ],
     'data': [
         'view/res_partner.xml',
         'security/ir.model.access.csv',
-    ]
+    ],
+    'demo': [
+    ],
 }
