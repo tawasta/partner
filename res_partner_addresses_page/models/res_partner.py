9805879f077d370e9e3ea014237064f36c53c94d
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-02-22 06:13:21 +0200

    Add a new module for splitting "Contacts&Addresses" to two pages

---
+++ res_partner_addresses_page/models/res_partner.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+# 1. Standard library imports:
+
+# 2. Known third party imports:
+
+# 3. Odoo imports (openerp):
+from odoo import api, fields, models
+
+# 4. Imports from Odoo modules:
+
+# 5. Local imports in the relative form:
+
+# 6. Unknown third party imports:
+
+
+class ResPartner(models.Model):
+    
+    # 1. Private attributes
+    _inherit = 'res.partner'
+
+    # 2. Fields declaration
+    contact_ids = fields.One2many(
+        comodel_name='res.partner',
+        inverse_name='parent_id',
+        string='Contacts',
+        domain=[
+            ('active', '=', True),
+            ('type', '=', 'contact'),
+        ]
+    )
+
+    address_ids = fields.One2many(
+        comodel_name='res.partner',
+        inverse_name='parent_id',
+        string='Contacts',
+        domain=[
+            ('active', '=', True),
+            ('type', '!=', 'contact'),
+        ]
+    )
+
+    # 3. Default methods
+
+    # 4. Compute and search fields, in the same order that fields declaration
+
+    # 5. Constraints and onchanges
+
+    # 6. CRUD methods
+
+    # 7. Action methods
+
+    # 8. Business methods
