636711a83dd9c8f4868fe58fd25da0e6aae41d38
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-03-20 15:44:28 +0200

    Refactor formatting

--- res_partner_addresses_page/models/res_partner.py
+++ res_partner_addresses_page/models/res_partner.py
@@ -5,7 +5,7 @@
 # 2. Known third party imports:
 
 # 3. Odoo imports (openerp):
-from odoo import api, fields, models
+from odoo import fields, models
 
 # 4. Imports from Odoo modules:
 
@@ -15,7 +15,7 @@ from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
-    
+
     # 1. Private attributes
     _inherit = 'res.partner'
 
@@ -42,7 +42,7 @@ class ResPartner(models.Model):
 
     # 3. Default methods
 
-    # 4. Compute and search fields, in the same order that fields declaration
+    # 4. Compute and search fields
 
     # 5. Constraints and onchanges
 
