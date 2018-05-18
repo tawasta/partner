9f453aeec66f0deeb2a5f8030b2f55ea761004d2
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2018-05-18 09:05:17 +0300

    Remove partner tree view filter override (moved to res_partner_tree_show_children)

--- res_partner_addresses_simple/__manifest__.py
+++ res_partner_addresses_simple/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Simplify partner address management',
     'summary': 'Simplify partner address management',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'category': 'CRM',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
