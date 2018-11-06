03f7591719b49a339d7ec3fe7ae8f5cecf5afa08
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2018-11-06 15:49:48 +0200

    [ADD] New module res_partner_profession

---
+++ res_partner_profession/models/res_profession.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+# 1. Standard library imports:
+
+# 2. Known third party imports:
+
+# 3. Odoo imports (openerp):
+from odoo import fields, models
+
+# 4. Imports from Odoo modules:
+
+# 5. Local imports in the relative form:
+
+# 6. Unknown third party imports:
+
+
+class ResProfession(models.Model):
+
+    # 1. Private attributes
+    _name = 'res.profession'
+    _order = 'name'
+
+    # 2. Fields declaration
+    name = fields.Char(
+        string="Name",
+        required=True,
+        translate=True
+    )
+    partner_ids = fields.Many2many(
+        comodel_name='res.partner',
+        relation='res_partner_res_profession_rel',
+        string='Partners',
+        help='Partners with this profession',
+    )
+
+    # 3. Default methods
+
+    # 4. Compute and search fields, in the same order that fields declaration
+
+    # 5. Constraints and onchanges
+    _sql_constraints = [
+        ('name',
+         'unique(name)',
+         'Please use unique profession names'),
+    ]
+
+    # 6. CRUD methods
+
+    # 7. Action methods
+
+    # 8. Business methods
