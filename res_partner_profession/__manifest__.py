9aa1b45909b447a7858840c6cacbb28eb8c14128
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2018-11-07 15:34:00 +0200

    [ADD] Added translations

--- res_partner_profession/__manifest__.py
+++ res_partner_profession/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Professions for partners',
     'summary': 'Adds professions to partners',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'category': 'CRM',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
