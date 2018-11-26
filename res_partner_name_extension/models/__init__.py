61601e231130404985d017d218204142e6e8d34f
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2018-11-26 12:54:30 +0200

    Created res_partner_name_extension module

---
+++ res_partner_name_extension/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from . import res_partner
+from . import account_invoice
+from . import sale_order
+from . import account_analytic_account
---
+++ res_partner_name_extension/models/account_invoice.py
@@ -0,0 +1,30 @@
+
+from openerp.osv import orm, fields
+from openerp.tools.translate import _
+
+class account_invoice(orm.Model):
+
+    _inherit = "account.invoice"
+
+    ''' Pull name extension suggestion from partner info '''
+    def onchange_partner_id(self, cr, uid, ids, type, partner_id,\
+            date_invoice=False, payment_term=False, partner_bank_id=False, company_id=False):
+
+        res =  super(account_invoice, self).onchange_partner_id(cr, uid, ids, type, partner_id,\
+            date_invoice,payment_term,partner_bank_id,company_id)
+        
+        ''' Only do this for customer invoices, not supplier invoices '''
+        if type == 'out_invoice':
+         
+            partner_model = self.pool.get('res.partner')
+            
+            ''' Check if the partner has a name extension defined '''
+            active_partner = partner_model.browse(cr, uid, [partner_id])[0]
+
+            res['value']['name_extension'] = active_partner.name_extension
+
+        return res
+
+    _columns = {
+        'name_extension': fields.char('Name extension', size=128)
+    }
