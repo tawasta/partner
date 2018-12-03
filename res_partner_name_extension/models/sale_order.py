df77533e7388d4c5553ee28e9f67c3fd87633747
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2018-12-03 11:00:02 +0200

    Modified structure to conforms Odoo rules

--- res_partner_name_extension/models/sale_order.py
+++ res_partner_name_extension/models/sale_order.py
@@ -7,17 +7,14 @@ class sale_order(models.Model):
 
     _inherit = 'sale.order'
 
-    def _make_invoice(self, cr, uid, order, lines, context=None):
+    def _make_invoice(self, order, lines, context=None):
 
-        res = super(sale_order, self)._make_invoice(cr, uid, order, lines)        
+        res = super(sale_order, self)._make_invoice(order, lines)        
 
         invoice_obj = self.pool.get('account.invoice')
         values = {
             'name_extension': order.partner_id.name_extension,
         }
 
-        invoice_obj.write(cr, uid, [res], values)
+        invoice_obj.write([res], values)
         return res
-
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
