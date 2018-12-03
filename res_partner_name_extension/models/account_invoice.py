b263468d547b55c3d6d47fe91a3945f565136c6c
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2018-12-03 13:05:06 +0200

    Modified structure

--- res_partner_name_extension/models/account_invoice.py
+++ res_partner_name_extension/models/account_invoice.py
@@ -9,12 +9,13 @@ class account_invoice(models.Model):
     # Pull name extension suggestion from partner info
     name_extension = fields.Char('Name extension', size=128)
 
-    def onchange_partner_id(self, type, partner_id,\
-            date_invoice=False, payment_term=False,
-            partner_bank_id=False, company_id=False):
+    def onchange_partner_id(self, type, partner_id,
+                            date_invoice=False, payment_term=False,
+                            partner_bank_id=False, company_id=False):
 
-        res = super(account_invoice, self).onchange_partner_id(type, partner_id, \
-            date_invoice, payment_term, partner_bank_id, company_id)
+        res = super(account_invoice, self).onchange_partner_id
+        (type, partner_id, date_invoice,
+         payment_term, partner_bank_id, company_id)
 
         # Only do this for customer invoices, not supplier invoices
         if type == 'out_invoice':
