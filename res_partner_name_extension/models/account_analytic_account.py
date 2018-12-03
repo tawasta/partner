af02083de570198530105b6ffead5f39ef4aa0da
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2018-12-03 11:00:02 +0200

    Modified structure to conforms Odoo rules

--- res_partner_name_extension/models/account_analytic_account.py
+++ res_partner_name_extension/models/account_analytic_account.py
@@ -6,10 +6,10 @@ class account_analytic_account(models.Model):
 
     _inherit = "account.analytic.account"
 
-    def _prepare_invoice_data(self, cr, uid, contract, context=None):
-        ''' Call super to get the invoice values provided by core modules, and then add some
-        additional ones '''
-        res = super(account_analytic_account, self)._prepare_invoice_data(cr, uid, contract, context)
+    def _prepare_invoice_data(self, contract):
+        # Call super to get the invoice values provided by core modules, and then add some
+        # additional ones
+        res = super(account_analytic_account, self)._prepare_invoice_data(contract)
         
         res.update({
             'name_extension': contract.partner_id.name_extension,
--- res_partner_name_extension/models/account_invoice.py
+++ res_partner_name_extension/models/account_invoice.py
@@ -6,20 +6,20 @@ class account_invoice(models.Model):
 
     _inherit = "account.invoice"
 
-    ''' Pull name extension suggestion from partner info '''
-    def onchange_partner_id(self, cr, uid, ids, type, partner_id,\
+    # Pull name extension suggestion from partner info
+    def onchange_partner_id(self, type, partner_id,\
             date_invoice=False, payment_term=False, partner_bank_id=False, company_id=False):
 
-        res =  super(account_invoice, self).onchange_partner_id(cr, uid, ids, type, partner_id,\
+        res =  super(account_invoice, self).onchange_partner_id(type, partner_id,\
             date_invoice,payment_term,partner_bank_id,company_id)
         
-        ''' Only do this for customer invoices, not supplier invoices '''
+        # Only do this for customer invoices, not supplier invoices
         if type == 'out_invoice':
          
             partner_model = self.pool.get('res.partner')
             
-            ''' Check if the partner has a name extension defined '''
-            active_partner = partner_model.browse(cr, uid, [partner_id])[0]
+            # Check if the partner has a name extension defined
+            active_partner = partner_model.browse([partner_id])[0]
 
             res['value']['name_extension'] = active_partner.name_extension
 
