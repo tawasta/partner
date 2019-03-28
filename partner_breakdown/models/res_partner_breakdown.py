7d512cc1d8e4486d6f53477405d18b133f3b7f45
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2019-03-28 16:15:28 +0200

    [ADD] Internal note (comment)

--- partner_breakdown/models/res_partner_breakdown.py
+++ partner_breakdown/models/res_partner_breakdown.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models
+from odoo import models, fields
 
 
 class ResPartnerBreakdown(models.Model):
@@ -7,3 +7,7 @@ class ResPartnerBreakdown(models.Model):
     _name = 'res.partner.breakdown'
 
     _inherits = {'res.partner': 'partner_id'}
+
+    comment = fields.Text(
+        string='Internal note',
+    )
