feca6ef0d7cfd2d4660b27528f585fb6ccd2ae02
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2019-03-28 12:35:11 +0200

    [ADD] Info texts

--- partner_breakdown_inventory/__manifest__.py
+++ partner_breakdown_inventory/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Partner breakdown: Inventory',
     'summary': 'Inventory (locations) to partner breakdown view',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'category': 'CRM',
     'website': 'https://tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
