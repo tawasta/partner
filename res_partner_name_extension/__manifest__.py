6ade9f22735a6fadd3aa7aed05b16c48950bbe52
retropikzel <retropikzel@mailbox.org>
2019-04-17 12:39:25 +0300

    Changed version number in manifest files to work with odoo 12

--- res_partner_name_extension/__manifest__.py
+++ res_partner_name_extension/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Name extension field for partner',
     'summary': 'Name extension field for partner',
-    'version': '10.0.1.0.0',
+    'version': '1.0.0',
     'category': 'CRM',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
