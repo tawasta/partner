c7887939103bbb9e19fa55a23b4432d4a080808e
retropikzel <retropikzel@mailbox.org>
2019-05-27 08:13:45 +0300

    [REM] Removed unnecessary utf-8 coding comments

--- res_partner_additional_terms/models/__init__.py
+++ res_partner_additional_terms/models/__init__.py
@@ -1,3 +1,2 @@
-# -*- coding: utf-8 -*-
 from . import additional_term
 from . import res_partner
--- res_partner_referenceright/models/__init__.py
+++ res_partner_referenceright/models/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 
 from . import reference_right
 from . import res_partner
