0ebc9abf70fbc9e6cd188774ac0f81ef301d2ad5
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2021-10-11 12:05:09 +0300

    Created new module

---
+++ res_partner_ssn/models/res_partner.py
@@ -0,0 +1,33 @@
+# 1. Standard library imports:
+
+# 2. Known third party imports:
+
+# 3. Odoo imports (openerp):
+from odoo import fields, models
+
+# 4. Imports from Odoo modules:
+
+# 5. Local imports in the relative form:
+
+# 6. Unknown third party imports:
+
+
+class ResPartner(models.Model):
+
+    # 1. Private attributes
+    _inherit = "res.partner"
+
+    # 2. Fields declaration
+    ssnid = fields.Char('SSN No', help='Social Security Number', groups="res_partner_ssn.group_ssn", tracking=True)
+
+    # 3. Default methods
+
+    # 4. Compute and search fields
+
+    # 5. Constraints and onchanges
+
+    # 6. CRUD methods
+
+    # 7. Action methods
+
+    # 8. Business methods
