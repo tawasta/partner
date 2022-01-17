a4ef8b772347b2e3c0f819e11c957c23d21b6526
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2022-01-17 15:30:46 +0200

    [ADD] "Accepted privacy consents"-field

---
+++ partner_privacy_consent/models/partner.py
@@ -0,0 +1,14 @@
+from odoo import fields
+from odoo import models
+
+
+class Partner(models.Model):
+
+    _inherit = "res.partner"
+
+    accepted_privacy_consent_ids = fields.One2many(
+        comodel_name="privacy.consent",
+        inverse_name="partner_id",
+        string="Accepted privacy consents",
+        domain=[("accepted", "=", True)],
+    )
