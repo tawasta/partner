6109a0fd22f71df0f0bd83b27fa676ace0cffe11
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2022-01-19 13:17:54 +0200

    [FIX] Missing permissions groups

--- partner_privacy_consent/__manifest__.py
+++ partner_privacy_consent/__manifest__.py
@@ -21,7 +21,7 @@
 {
     "name": "Partner privacy consent",
     "summary": "Adds privacy consent helpers for partner",
-    "version": "14.0.1.1.0",
+    "version": "14.0.1.1.1",
     "category": "CRM",
     "website": "https://github.com/Tawasta/partner",
     "author": "Tawasta",
