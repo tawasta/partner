1fcacefaf0f5bd847da0c013f8679f43c5bd9cb6
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-03-06 14:29:12 +0200

    Modified function

--- partner_name_and_id/models/res_partner.py
+++ partner_name_and_id/models/res_partner.py
@@ -8,6 +8,6 @@ class ResPartner(models.Model):
     def name_get(self):
         result = []
         for record in self:
-            record_name = record.name + " - " + record.id
+            record_name = record.name + " - " + str(record.id)
             result.append((record.id, record_name))
         return result
