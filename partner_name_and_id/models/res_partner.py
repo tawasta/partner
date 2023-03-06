c9b367e5546f455fa6387d0645e3d37b41df6154
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-03-06 14:24:51 +0200

    Created new module

---
+++ partner_name_and_id/models/res_partner.py
@@ -0,0 +1,13 @@
+from odoo import api, models
+
+
+class ResPartner(models.Model):
+
+    _inherit = "res.partner"
+
+    def name_get(self):
+        result = []
+        for record in self:
+            record_name = record.name + " - " + record.id
+            result.append((record.id, record_name))
+        return result
