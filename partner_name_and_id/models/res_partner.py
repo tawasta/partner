0fde6552188141565238d16c468aa0eaf9374a7e
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-03-06 14:46:27 +0200

    Modified function

--- partner_name_and_id/models/res_partner.py
+++ partner_name_and_id/models/res_partner.py
@@ -11,13 +11,3 @@ class ResPartner(models.Model):
             record_name = record.name + " - " + str(record.id)
             result.append((record.id, record_name))
         return result
-
-    @api.model
-    def name_search(self, name, args=None, operator="ilike", limit=100):
-        args = args or []
-        recs = self.search([("id", operator, name)] + args, limit=limit)
-        if not recs.ids:
-            return super(OpStudent, self).name_search(
-                name=name, args=args, operator=operator, limit=limit
-            )
-        return recs.name_get()
