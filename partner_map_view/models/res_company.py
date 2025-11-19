cddf7af633965e6b895f21702232c410029e04da
joona-isoaho-futural <joona.isoaho@futural.fi>
2025-11-19 08:01:19 +0200

    Take latitude and longitude for company from res.partner associated with company

--- partner_map_view/models/res_company.py
+++ partner_map_view/models/res_company.py
@@ -4,5 +4,5 @@ from odoo import fields, models
 class ResCompany(models.Model):
     _inherit = "res.company"
 
-    company_latitude = fields.Float(string="Latitude", default=0)
-    company_longitude = fields.Float(string="Longitude", default=0)
+    company_latitude = fields.Float(string="Latitude", related="partner_id.partner_latitude")
+    company_longitude = fields.Float(string="Longitude", related="partner_id.partner_longitude")
