32fd285f4d5cfdee1dbeb06f473a86a991935078
joona-isoaho-futural <joona.isoaho@futural.fi>
2025-11-25 10:03:48 +0200

    Precommit fixes

--- partner_map_view/static/src/js/partner_map_controller.esm.js
+++ partner_map_view/static/src/js/partner_map_controller.esm.js
@@ -5,7 +5,6 @@ import {
     onMounted,
     onWillStart,
     onWillUpdateProps,
-    useState,
 } from "@odoo/owl";
 import {Layout} from "@web/search/layout";
 import {SearchBar} from "@web/search/search_bar/search_bar";
