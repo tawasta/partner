00bc351251321099331ff480a408bc957fbc8fc6
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2026-06-03 09:17:20 +0300

    [IMP] - added function to delete chatter messages

--- partner_anonymization/models/partner.py
+++ partner_anonymization/models/partner.py
@@ -7,6 +7,26 @@ class ResPartner(models.Model):
 
     _inherit = "res.partner"
 
+    def _get_chatter_messages_domain(self):
+        self.ensure_one()
+        return [
+            "|",
+            "|",
+            "&",
+            ("model", "=", "res.partner"),
+            ("res_id", "=", self.id),
+            ("author_id", "=", self.id),
+            ("partner_ids", "in", [self.id]),
+        ]
+
+    def _delete_chatter_messages(self):
+        self.ensure_one()
+        messages = self.env["mail.message"].sudo().search(
+            self._get_chatter_messages_domain()
+        )
+        if messages:
+            messages.unlink()
+
     def anonymize(self):
         """
         Anonymize a partner
@@ -52,6 +72,8 @@ class ResPartner(models.Model):
                 if record.mass_mailing_contact_ids:
                     for m in record.mass_mailing_contact_ids:
                         m.unlink()
+
+            record._delete_chatter_messages()
 
             values["name"] = user_hash
             record.write(values)
