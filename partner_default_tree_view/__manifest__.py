e6f54855e6996e20b5b38e8456e5754c46f6f77c
joona-isoaho-futural <joona.isoaho@futural.fi>
2026-06-23 16:56:41 +0300

    [MIG] 19.0 partner_default_tree_view

---
+++ partner_default_tree_view/__manifest__.py
@@ -0,0 +1,33 @@
+##############################################################################
+#
+#    Author: Futural Oy
+#    Copyright 2018 Futural Oy (https://futural.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    "name": "Partner Default Tree View",
+    "summary": "Defaults Contacts action to list view",
+    "version": "19.0.1.0.0",
+    "category": "Contacts",
+    "website": "https://github.com/tawasta/partner",
+    "author": "Futural",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["account", "contacts"],
+    "data": ["data/ir_model_action.xml"],
+}
