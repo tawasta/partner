87b8791a9415a024e3efa5ba7325bf454723c97c
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2026-09-22 07:57:47 +0300

    [MIG] - modified module to 19 version

---
+++ partner_factoring_contract/__manifest__.py
@@ -0,0 +1,42 @@
+##############################################################################
+#
+#    Author: Futural Oy
+#    Copyright 2026 Futural Oy (https://futural.fi)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see http://www.gnu.org/licenses/agpl.html
+#
+##############################################################################
+
+{
+    "name": "Partner Factoring Contract",
+    "summary": "Generic factoring contract on partners and invoices",
+    "version": "19.0.1.0.0",
+    "category": "Sales",
+    "website": "https://github.com/tawasta/partner",
+    "author": "Futural",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {"python": [], "bin": []},
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/factoring_contract_views.xml",
+        "views/res_partner_views.xml",
+        "views/account_move_views.xml",
+    ],
+    "demo": [],
+}
