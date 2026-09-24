71c53c1818091fc595aac30720050037f16e2299
Joonas Lahtinen <joonas.lahtinen@futural.fi>
2026-09-24 16:47:04 +0300

    [IMP] readjusted some of the tables and added photo button to portal

--- partner_membership_data_card/controllers/controller.py
+++ partner_membership_data_card/controllers/controller.py
@@ -4,12 +4,14 @@ from odoo.http import request
 
 class MembershipPortal(http.Controller):
     @http.route("/my/membership-card", type="http", auth="user", website=True)
-    def membership_card(self, **kwargs):
+    def membership_card(self, photo=False, **kwargs):
         partner = request.env.user.partner_id
+        with_photo = str(photo).lower() in ("1", "true", "yes", "on")
 
         pdf_content, _ = (
             request.env["ir.actions.report"]
             .sudo()
+            .with_context(with_photo=with_photo)
             ._render_qweb_pdf(
                 "partner_membership_data_card.partner_card_template", partner.ids
             )
